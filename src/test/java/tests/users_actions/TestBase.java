9e6bcaac699a952682a834d1827ba302252b9c4d
dinosaturnus <alinakrivosheeva99@gmail.com>
2023-07-08 14:47:19 +0300

    initial commit

--- src/test/java/tests/users_actions/TestBase.java
+++ src/test/java/tests/users_actions/TestBase.java
@@ -1,12 +1,12 @@
 package tests.users_actions;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 
 public abstract class TestBase {
 
-    @BeforeEach
-    void beforeEach() {
+    @BeforeAll
+    static void beforeAll() {
         RestAssured.baseURI = "https://reqres.in";
         RestAssured.basePath = "/api";
     }
